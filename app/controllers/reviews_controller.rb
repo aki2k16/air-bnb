dbca3e4a452942cdba700549d0b889e5b35e1367
aki2k16 <kobayashi.a1987@tbz.t-com.ne.jp>
2017-03-04 12:36:03 +0900

    edit controller.

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,18 @@
+class ReviewsController < ApplicationController
+
+  def new
+    @room = Room.find(params[:room_id])
+    @review = Review.new
+  end
+
+  def create
+    @room = Room.find(params[:room_id])
+    @review = current_user.reviews.create(review_params)
+    redirect_to room_path(params[:room_id])
+  end
+
+  private
+  def review_params
+    params.require(:review).permit(:body).merge(room_id: params[:room_id])
+  end
+end
--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,5 +2,10 @@ class RoomsController < ApplicationController
 
   def index
   end
-  
+
+  def show
+    @room = Room.find(params[:id])
+    @reviews = @room.reviews
+  end
+
 end
