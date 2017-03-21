f12b8e6c48344f89a0cd484225841c95ceba3244
aki2k16 <kobayashi.a1987@tbz.t-com.ne.jp>
2017-03-21 13:38:15 +0900

    Add "from_omniauth" method in user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,28 +9,16 @@ class User < ApplicationRecord
   has_many :rooms
   has_many :reservations
   has_many :social_profiles, dependent: :destroy
-  def social_profile(provider)
-    social_profiles.select{ |sp| sp.provider == provider.to_s }.first
+  
+  def self.from_omniauth(auth)
+  where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+    user.email = auth.info.email
+    user.password = Devise.friendly_token[0,20]
+    user.name = auth.info.name   # assuming the user model has a name
+    user.image = auth.info.image # assuming the user model has an image
+    # If you are using confirmable and the provider(s) you use validate emails,
+    # uncomment the line below to skip the confirmation emails.
+    # user.skip_confirmation!
   end
-
-  def self.find_for_oauth(auth)
-    user = User.where(uid: auth.uid, provider: auth.provider).first
-
-    unless user
-      user = User.create(
-        uid:      auth.uid,
-        provider: auth.provider,
-        email:    User.dummy_email(auth),
-        password: Devise.friendly_token[0, 20]
-      )
-    end
-
-    user
-  end
-
-  private
-
-  def self.dummy_email(auth)
-    "#{auth.uid}-#{auth.provider}@example.com"
-  end
+end
 end
