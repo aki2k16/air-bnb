e20281543dd9d0941dceec3dcc38ee8ffd605a7e
aki2k16 <kobayashi.a1987@tbz.t-com.ne.jp>
2017-03-28 17:56:06 +0900

    Edit index.html.erb.

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,7 +12,7 @@ set :rbenv_ruby, '2.3.1'
 set :keep_releases, 5
 
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['/Users/aki/.ssh/aws.pem']
+                  keys: ['/Users/aki/.ssh/air-bnb.pem']
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-server '54.65.213.40', user: 'ec2-user', roles: %w{app db web}
+server '13.113.105.246', user: 'ec2-user', roles: %w{app db web}
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
