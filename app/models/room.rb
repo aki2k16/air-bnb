55c6f7a3474e15f5214cc358aa7a2004509239fa
aki2k16 <kobayashi.a1987@tbz.t-com.ne.jp>
2017-04-08 16:27:35 +0900

    Add "address" to validation.

--- app/models/room.rb
+++ app/models/room.rb
@@ -4,9 +4,9 @@ class Room < ApplicationRecord
   has_many :reservations
 
   # バリデーション
-  validates :price, :bed, :bedroom, :listing, :prefecture, :guest, :title, presence: true
+  validates :price, :address, :bed, :bedroom, :listing, :prefecture, :guest, :title, presence: true
 
   #adressから緯度と経度を取得。
-  geocoded_by :address   
+  geocoded_by :address
   after_validation :geocode
 end
